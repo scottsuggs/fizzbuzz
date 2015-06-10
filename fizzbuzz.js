3215f611db6a865a0031d867c054ba6af936a63a
Wilbur Q. Diggs <wilburdiggs@gmail.com>
2015-06-09 21:47:37 -0600

    Completed FizzBuzz

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,40 @@
-for (var i = 1; i <= 100; i++) {
+var number = 100;
+
+for (var i = 1; i <= number; i++) {
+
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log("FizzBuzz");
+    var para = document.createElement("p");
+	var node = document.createTextNode("FizzBuzz");
+	para.appendChild(node);
+	para.className = "fizzBuzz";
+	var element = document.getElementById("Main");
+	element.appendChild(para);    
     }
+
     else if (i % 5 === 0) {
-        console.log("Buzz");
+    var para = document.createElement("p");
+	var node = document.createTextNode("Buzz");
+	para.appendChild(node);
+	para.className = "buzz";
+	var element = document.getElementById("Main");
+	element.appendChild(para);
     }
+
     else if (i % 3 === 0) {
-        console.log("Fizz");
+	var para = document.createElement("p");
+	var node = document.createTextNode("Fizz");
+	para.appendChild(node);
+	para.className = "fizz";
+	var element = document.getElementById("Main");
+	element.appendChild(para);
     }
+
     else {
-        console.log(i);
+    var para = document.createElement("p");
+	var node = document.createTextNode(i);
+	para.appendChild(node);
+	para.className = "i";
+	var element = document.getElementById("Main");
+	element.appendChild(para);
     }
 }
